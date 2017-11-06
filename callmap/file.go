73ee30199adbef411e3ecd8988c5527152d5bb3e
Alexander Döring <email@alexd.ch>
2017-11-06 22:40:50 +0100

    start attempt with babylon parser

--- callmap/file.go
+++ callmap/file.go
@@ -1,89 +1,93 @@
 package callmap
 
 import (
-	"bytes"
-	"io"
-	"io/ioutil"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
 )
 
-// File describes a javascript file.
 type File struct {
-	functions map[string]*Function
-	src       []byte
-	pos       int
+	Program Program
+}
+
+type Program struct {
+	Body []map[string]interface{} `json:"body"`
 }
 
 func newFile(path string) (*File, error) {
-	src, err := ioutil.ReadFile(path)
+	out, err := exec.Command("babylon", path).CombinedOutput()
 	if err != nil {
+		log.Printf("err: %s", out)
 		return nil, err
 	}
 
-	f := &File{
-		functions: make(map[string]*Function),
-		src:       src,
-	}
-	if err := f.parse(); err != nil {
+	var f File
+	if err := json.Unmarshal(out, &f); err != nil {
 		return nil, err
 	}
-	return f, nil
+
+	for _, node := range f.Program.Body {
+		walk(node)
+	}
+
+	return &f, nil
 }
 
-func (f *File) parse() error {
-	for ; f.pos < len(f.src); f.pos++ {
-		switch f.src[f.pos] {
-		case '/':
-			if err := f.maybeComment(); err != nil {
-				return err
-			}
-		case 'f':
-			if err := f.maybeFunction(); err != nil {
-				return err
-			}
+func walk(node map[string]interface{}) {
+	switch node["type"] {
+	case "AssignmentExpression":
+		right := node["right"].(map[string]interface{})
+		walk(right)
+	case "BinaryExpression":
+		left := node["left"].(map[string]interface{})
+		walk(left)
+		right := node["right"].(map[string]interface{})
+		walk(right)
+	case "BlockStatement":
+		body := node["body"].([]interface{})
+		for _, n := range body {
+			n2 := n.(map[string]interface{})
+			walk(n2)
 		}
-	}
-
-	return nil
-}
-
-func (f *File) maybeComment() error {
-	switch f.src[f.pos+1] {
-	case '*':
-		i := bytes.Index(f.src[f.pos+2:], []byte("*/"))
-		if i == -1 {
-			return io.ErrUnexpectedEOF
+	case "ForStatement":
+		init := node["init"].(map[string]interface{})
+		walk(init)
+		test := node["test"].(map[string]interface{})
+		walk(test)
+		update := node["update"].(map[string]interface{})
+		walk(update)
+		body := node["body"].(map[string]interface{})
+		walk(body)
+	case "ReturnStatement":
+		argument := node["argument"].(map[string]interface{})
+		walk(argument)
+	case "ExpressionStatement":
+		node2 := node["expression"].(map[string]interface{})
+		walk(node2)
+	case "CallExpression":
+		callee := node["callee"].(map[string]interface{})
+		if callee["type"] == "MemberExpression" {
+			object := callee["object"].(map[string]interface{})
+			property := callee["property"].(map[string]interface{})
+			fmt.Printf("call %s.%s()\n", object["name"], property["name"])
 		}
-		f.pos += 2 + i + 1
-	case '/':
-		i := bytes.IndexByte(f.src[f.pos+2:], '\n')
-		if i == -1 {
-			return io.ErrUnexpectedEOF
+		if callee["type"] == "Identifier" {
+			fmt.Printf("call %s()\n", callee["name"])
 		}
-		f.pos += 2 + i
+		nodes := node["arguments"].([]interface{})
+		for _, n := range nodes {
+			n2 := n.(map[string]interface{})
+			walk(n2)
+		}
+	case "FunctionDeclaration":
+		id := node["id"].(map[string]interface{})
+		fmt.Printf("%s(){\n", id["name"])
+		body := node["body"].(map[string]interface{})
+		walk(body)
+		fmt.Printf("}\n")
+	case "Identifier", "NumericLiteral", "UpdateExpression", "MemberExpression":
+	default:
+		log.Print(node["type"])
 	}
-
-	return nil
-}
-
-func (f *File) maybeFunction() error {
-	if !bytes.HasPrefix(f.src[f.pos:], needle) {
-		return nil
-	}
-
-	fn, err := parseFunction(f.src, f.pos)
-	if err != nil {
-		return err
-	}
-
-	f.functions[fn.Name] = fn
-	f.pos = fn.End
-	return nil
-}
-
-func (f File) String() string {
-	str := "functions: "
-	for _, fn := range f.functions {
-		str += fn.String() + " "
-	}
-	return str
 }
