40dff3fe207f27026c09e1ca7135ee95b500f1ec
Alexander Döring <email@alexd.ch>
2017-11-07 21:59:17 +0100

    callmap: handle a lot more types

--- callmap/file.go
+++ callmap/file.go
@@ -33,15 +33,32 @@ func walk(node interface{}) {
 		return
 	}
 
-	n := node.(map[string]interface{})
+	n, ok := node.(map[string]interface{})
+	if !ok {
+		return
+	}
 	switch n["type"] {
 
 	case "AssignmentExpression":
+		walk(n["left"])
 		walk(n["right"])
+
+	case "ArrayExpression":
+		walk(n["elements"])
+
+	case "ArrowFunctionExpression":
+		walk(n["body"])
+
+	case "AwaitExpression":
+		walk(n["argument"])
 
 	case "BinaryExpression":
 		walk(n["left"])
 		walk(n["right"])
+
+	case "BindExpression":
+		walk(n["object"])
+		walk(n["callee"])
 
 	case "BlockStatement":
 		walk(n["body"])
@@ -58,11 +75,36 @@ func walk(node interface{}) {
 		}
 		walk(n["arguments"])
 
+	case "CatchClause":
+		walk(n["param"])
+		walk(n["body"])
+
+	case "Class":
+		walk(n["superClass"])
+		walk(n["body"])
+
+	case "ConditionalExpression":
+		walk(n["test"])
+		walk(n["consequent"])
+		walk(n["alternate"])
+
+	case "DoExpression":
+		walk(n["body"])
+
+	case "DoWhileStatement":
+		walk(n["body"])
+		walk(n["test"])
+
 	case "ExpressionStatement":
 		walk(n["expression"])
 
 	case "File":
 		walk(n["program"])
+
+	case "ForInStatement":
+		walk(n["left"])
+		walk(n["right"])
+		walk(n["body"])
 
 	case "ForStatement":
 		walk(n["init"])
@@ -76,16 +118,107 @@ func walk(node interface{}) {
 		walk(n["body"])
 		fmt.Printf("}\n")
 
+	case "IfStatement":
+		walk(n["test"])
+		walk(n["consequent"])
+		walk(n["alternate"])
+
+	case "LabeledStatement":
+		walk(n["body"])
+
+	case "LogicalExpression":
+		walk(n["left"])
+		walk(n["right"])
+
+	case "MemberExpression":
+		walk(n["object"])
+		walk(n["property"])
+
+	case "NewExpression":
+		callee := n["callee"].(map[string]interface{})
+		if callee["type"] == "MemberExpression" {
+			object := callee["object"].(map[string]interface{})
+			property := callee["property"].(map[string]interface{})
+			fmt.Printf("call %s.%s()\n", object["name"], property["name"])
+		}
+		if callee["type"] == "Identifier" {
+			fmt.Printf("call %s()\n", callee["name"])
+		}
+		walk(n["arguments"])
+
+	case "ObjectExpression":
+		walk(n["properties"])
+
+	case "ObjectProperty":
+		walk(n["value"])
+
 	case "Program":
 		walk(n["body"])
 
 	case "ReturnStatement":
 		walk(n["argument"])
 
-	case "Identifier",
-		"MemberExpression",
+	case "SequenceExpression":
+		walk(n["expressions"])
+
+	case "SpreadElement":
+		walk(n["argument"])
+
+	case "SwitchCase":
+		walk(n["test"])
+		walk(n["consequent"])
+
+	case "SwitchStatement":
+		walk(n["discriminant"])
+		walk(n["cases"])
+
+	case "TaggedTemplateExpression":
+		walk(n["tag"])
+
+	case "TemplateLiteral":
+		walk(n["expressions"])
+
+	case "ThrowStatement":
+		walk(n["argument"])
+
+	case "TryStatement":
+		walk(n["block"])
+		walk(n["handler"])
+		walk(n["finalizer"])
+
+	case "UnaryExpression":
+		walk(n["argument"])
+
+	case "UpdateExpression":
+		walk(n["argument"])
+
+	case "VariableDeclaration":
+		walk(n["declarations"])
+
+	case "VariableDeclarator":
+		walk(n["init"])
+
+	case "WhileStatement":
+		walk(n["test"])
+		walk(n["body"])
+
+	case "WithStatement":
+		walk(n["object"])
+		walk(n["body"])
+
+	case "YieldExpression":
+		walk(n["argument"])
+
+	case
+		"BooleanLiteral",
+		"BreakStatement",
+		"ContinueStatement",
+		"EmptyStatement",
+		"ForOfStatement",
+		"DebuggerStatement",
+		"Identifier",
 		"NumericLiteral",
-		"UpdateExpression":
+		"StringLiteral":
 
 	default:
 		log.Print(n["type"])
