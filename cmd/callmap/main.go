2b837e16077b0195814ab5472b1573235b65bac7
Alexander Döring <email@alexd.ch>
2017-11-13 21:42:55 +0100

    cmd/callmap: count imports, functions, calls

--- cmd/callmap/main.go
+++ cmd/callmap/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/alexd765/jsbundler/callmap"
 )
@@ -18,11 +19,25 @@ func main() {
 		log.Fatal("err: Needs javascript source files as parameters.")
 	}
 
+	start := time.Now()
+
 	cm, err := callmap.New(flag.Args()...)
 	if err != nil {
 		log.Fatalf("err: %s", err)
 	}
-	log.Printf("callmap parsed %d javascript files", len(cm.Files))
+	fmt.Printf("Parsed %d javascript files in %.1f seconds.\n", len(cm.Files), time.Since(start).Seconds())
+	var im, fn, ca int
+	for _, f := range cm.Files {
+		im += len(f.Imports)
+		fn += len(f.Functions)
+		ca += len(f.Calls)
+		for _, childFn := range f.Functions {
+			fn2, ca2 := count(childFn)
+			fn += fn2
+			ca += ca2
+		}
+	}
+	fmt.Printf("Found %d import statements, %d function declarations and %d function calls.\n", im, fn, ca)
 
 	if *v {
 		out, err := json.MarshalIndent(cm, "", "  ")
@@ -31,4 +46,15 @@ func main() {
 		}
 		fmt.Print(string(out))
 	}
+}
+
+func count(fn *callmap.Function) (countFn int, countCa int) {
+	for _, childFn := range fn.Functions {
+		childCountFn, childCountCa := count(childFn)
+		countFn += childCountFn
+		childCountCa += childCountCa
+	}
+	countFn += len(fn.Functions)
+	countCa += len(fn.Calls)
+	return countFn, countCa
 }
